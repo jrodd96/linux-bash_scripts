35b2cb42af3fcbd0d485ecd0a6196ed53a4d144a
Jared Stevens <jared.stevens1229@gmail.com>
2019-09-23 10:44:35 -0400

    upload update.sh script

---
+++ apt-update/update.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+check_exit_status() {
+
+    if [ $? -eq 0 ]
+    then
+        echo
+        echo "Success"
+        echo
+    else
+        echo
+        echo "[ERROR] Process Failed!"
+        echo
+		
+        read -p "The last command exited with an error. Exit script? (yes/no) " answer
+
+        if [ "$answer" == "yes" ]
+        then
+            exit 1
+        fi
+    fi
+}
+
+greeting() {
+
+    echo
+    echo "Welcome back, $USER. Executing system update commands..."
+    echo
+}
+
+update() {
+
+    sudo apt-get update;
+    check_exit_status
+
+    sudo apt-get upgrade -y;
+    check_exit_status
+
+    sudo apt-get dist-upgrade -y;
+    check_exit_status
+}
+
+housekeeping() {
+
+    sudo apt-get autoremove -y;
+    check_exit_status
+
+    sudo apt-get autoclean -y;
+    check_exit_status
+}
+
+leave() {
+
+    echo
+    echo "----------------------"
+    echo "- Updates Successful -"
+    echo "----------------------"
+    echo
+    exit
+}
+
+greeting
+update
+housekeeping
+leave
+
+
